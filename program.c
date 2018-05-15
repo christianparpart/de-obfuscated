b1b559dae873034cb6d71f78d841d0e47999fbb3
Christian Parpart <christian@parpart.family>
2018-05-15 11:10:33 +0200

    s

--- program.c
+++ program.c
@@ -17,11 +17,11 @@ int main(int t, int _, char* a) {
     } else {
       return 16;
     }
-  } else if (t < 0) {                             // #6
-    if (t < -72) {                                // #7
+  } else if (t < 0) {                               // #6
+    if (t < -72) {                                  // #7
       return main(_, t, "@n'+,#'/*{}w+/w#cdnr/+,{}r/*de}+,/*{*+,/w{%+,/w#q#n+,/#{l,+,/n{n+,/+#n+,/#;#q#n+,/+k#;*+,/'r :'d*'3,}{w+K w'K:'+}e#';dq#'l q#'+d'K#!/+k#;q#'r}eKK#}w'r}eKK{nl]'/#;#q#n'){)#}w'){){nl]'/+#n';d}rw' i;# ){nl]!/n{n#'; r{#w'r nc{nl]'/#{l,+'K {rw' iK{;[{nl]'/w#q#n'wk nw' iwk{KK{nl]!/w{%'l##w#' i; :{nl]'/*{q#'ld;r'}{nlwb!/*de}'c ;;{nl'-{}rw]'/+,}##'*}#nc,',#nw]'/+kd'+e}+;#'rdq#w! nr'/ ') }+}{rl#'{n' ')# }'+}##(!!/");
-    } else if (t < -50) {                         // #8
-      if (_ == *a) {                              // #9
+    } else if (t < -50) {                           // #8
+      if (_ == *a) {                                // #9
         return putchar(a[31]);
       } else {
         return main(-65, _, a + 1);
@@ -29,9 +29,9 @@ int main(int t, int _, char* a) {
     } else {
       return main((*a == '/') + t, _, a + 1);
     }
-  } else if (0 < t) {                             // #10
+  } else if (0 < t) {                               // #10
     return main(2, 2, "%s");
-  } else if (*a != '/') {                         // #11
+  } else if (*a != '/') {                           // #11
     main(0,
          main(-61,
               *a,
